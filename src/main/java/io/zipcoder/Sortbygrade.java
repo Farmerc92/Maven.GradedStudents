a2e5e384621a819f882885d8b934ba075953e4c2
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-19 21:25:49 -0500

    help I am dying

--- src/main/java/io/zipcoder/Sortbygrade.java
+++ src/main/java/io/zipcoder/Sortbygrade.java
@@ -6,6 +6,6 @@ public class Sortbygrade implements Comparator<Student> {
 
     @Override
     public int compare(Student o1, Student o2) {
-        return Double.compare(o1.getAverageExamScore(), o2.getAverageExamScore());
+        return Double.compare(o2.getAverageExamScore(), o1.getAverageExamScore());
     }
 }

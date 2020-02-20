9193762c48a3cee8a3c66b4a09f19618cc1251d0
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-20 12:04:25 -0500

    I challenge to read my last method and test in classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,8 +1,8 @@
 package io.zipcoder;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 
 public class Classroom {
     private Student[] students;
@@ -70,6 +70,39 @@ public class Classroom {
         Collections.sort(array, new Sortbygrade());
         return array.toArray(new Student[students.length]);
 
+    }
+
+    public HashMap<Character, ArrayList<Student>> getGradeBook(){
+        HashMap<Character, ArrayList<Student>> studentGradeMap = new HashMap<>();
+        ArrayList<Student> studentsA = new ArrayList<>();
+        ArrayList<Student> studentsB = new ArrayList<>();
+        ArrayList<Student> studentsC = new ArrayList<>();
+        ArrayList<Student> studentsD = new ArrayList<>();
+        ArrayList<Student> studentsF = new ArrayList<>();
+        Double highestGrade = this.getStudentsByScore()[0].getAverageExamScore();
+        for (Student s: students) {
+            if(s.getAverageExamScore() >= highestGrade * 0.9){
+                studentsA.add(s);
+            }
+            else if (s.getAverageExamScore() < highestGrade * 0.9 && s.getAverageExamScore() > highestGrade * 0.7){
+                studentsB.add(s);
+            }
+            else if (s.getAverageExamScore() <= highestGrade * 0.7 && s.getAverageExamScore() > highestGrade * 0.5){
+                studentsC.add(s);
+            }
+            else if (s.getAverageExamScore() <= highestGrade * 0.5 && s.getAverageExamScore() > highestGrade * 0.1){
+                studentsD.add(s);
+            }
+            else{
+                studentsF.add(s);
+            }
+        }
+        studentGradeMap.put('A', studentsA);
+        studentGradeMap.put('B', studentsB);
+        studentGradeMap.put('C', studentsC);
+        studentGradeMap.put('D', studentsD);
+        studentGradeMap.put('F', studentsF);
+        return studentGradeMap;
     }
 
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 public class ClassroomTest {
@@ -144,5 +145,61 @@ public class ClassroomTest {
         Assert.assertEquals(student, byScore[1]);
         Assert.assertEquals(student1, byScore[2]);
         Assert.assertEquals(student2, byScore[3]);
+    }
+
+    @Test
+    public void getGradeBookTest(){
+        Double[] examScores = { 100.0 };
+        Student student = new Student("Leon", "Hunter", examScores);
+
+        Double[] examScores1 = { 89.0 };
+        Student student1 = new Student("Bob", "Dole", examScores1);
+
+        Student student2 = new Student("Steve", "Jackson", examScores1);
+
+        Double[] examScores3 = { 69.0 };
+        Student student3 = new Student("John", "Guy", examScores3);
+
+        Double[] examScores4 = { 49.0 };
+        Student student4 = new Student("Dexter", "Lab", examScores4);
+
+        Double[] examScores5 = { 5.0 };
+        Student student5 = new Student("Jimmy", "Neutron", examScores5);
+
+        Student[] studentArray = new Student[6];
+        studentArray[0] = student;
+        studentArray[1] = student1;
+        studentArray[2] = student2;
+        studentArray[3] = student3;
+        studentArray[4] = student4;
+        studentArray[5] = student5;
+
+        Classroom classGrade = new Classroom(studentArray);
+        HashMap<Character, ArrayList<Student>> actual = new HashMap<>();
+        HashMap<Character, ArrayList<Student>> expected = classGrade.getGradeBook();
+        ArrayList<Student> studentsTestA = new ArrayList<>();
+        ArrayList<Student> studentsTestB = new ArrayList<>();
+        ArrayList<Student> studentsTestC = new ArrayList<>();
+        ArrayList<Student> studentsTestD = new ArrayList<>();
+        ArrayList<Student> studentsTestF = new ArrayList<>();
+        studentsTestA.add(student);
+        studentsTestB.add(student1);
+        studentsTestB.add(student2);
+        studentsTestC.add(student3);
+        studentsTestD.add(student4);
+        studentsTestF.add(student5);
+
+        actual.put('A', studentsTestA);
+        actual.put('B', studentsTestB);
+        actual.put('C', studentsTestC);
+        actual.put('D', studentsTestD);
+        actual.put('F', studentsTestF);
+
+        Assert.assertEquals(expected.get('A').get(0), actual.get('A').get(0));
+        Assert.assertEquals(expected.get('B').get(0), actual.get('B').get(0));
+        Assert.assertEquals(expected.get('B').get(1), actual.get('B').get(1));
+        Assert.assertEquals(expected.get('C').get(0), actual.get('C').get(0));
+        Assert.assertEquals(expected.get('D').get(0), actual.get('D').get(0));
+        Assert.assertEquals(expected.get('F').get(0), actual.get('F').get(0));
     }
 }
